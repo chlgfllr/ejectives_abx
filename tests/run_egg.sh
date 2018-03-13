5fa822f2abd58aea6b220dfd34f09238d33f6908
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-13 08:12:17 -0400

    edit egg

--- tests/run_egg.sh
+++ tests/run_egg.sh
@@ -5,7 +5,7 @@ set -e
 source activate easyabx
 
 TEST=egg_fixed
-FOLDER=by__everything_boston
+FOLDER=by__essai_boston
 DIR=../results/$TEST
 
 NAME=abx
@@ -20,7 +20,7 @@ echo
 
 mkdir $DIR/$FOLDER && \
   ~/easy_abxpy/easy_abx/prepare_abx.py $DIR/egg_edit.csv $DIR/$FOLDER/$NAME --header \
-     --col_features 14-15 --col_labels 6-12 --col_items 1,2,3 && \
+     --col_features 14-38 --col_labels 6-12 --col_items 1,2,3 && \
   ~/easy_abxpy/easy_abx/run_abx.py $DIR/$FOLDER/$NAME --on "type" --by place position question speaker vowel &&\
   python dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT && \
   cp $DIR/$FOLDER/abx.csv $DIR/$FOLDER/results.csv; rm $DIR/$FOLDER/abx.csv && \
