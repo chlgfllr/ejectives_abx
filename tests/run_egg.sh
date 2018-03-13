3727134bc2558b8f6906a9f4cbb709e070630f8b
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-13 08:00:42 -0400

    add run_egg.sh

--- tests/run_egg.sh
+++ tests/run_egg.sh
@@ -4,7 +4,7 @@ set -e
 
 source activate easyabx
 
-TEST=features
+TEST=egg_fixed
 FOLDER=by__everything_boston
 DIR=../results/$TEST
 
@@ -19,7 +19,7 @@ echo "Running easyabx test and extracting distances"
 echo
 
 mkdir $DIR/$FOLDER && \
-  ~/easy_abxpy/easy_abx/prepare_abx.py $DIR/abx.csv $DIR/$FOLDER/$NAME --header \
+  ~/easy_abxpy/easy_abx/prepare_abx.py $DIR/egg_edit.csv $DIR/$FOLDER/$NAME --header \
      --col_features 14-15 --col_labels 6-12 --col_items 1,2,3 && \
   ~/easy_abxpy/easy_abx/run_abx.py $DIR/$FOLDER/$NAME --on "type" --by place position question speaker vowel &&\
   python dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT && \
