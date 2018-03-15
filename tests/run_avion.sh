c6507906bb653d093f71017f400c52c23055dde3
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-15 09:11:05 +0000

    test features and egg

--- tests/run_avion.sh
+++ tests/run_avion.sh
@@ -18,14 +18,17 @@ echo
 echo "Running easyabx test and extracting distances"
 echo
 
+rm -rf .cache
+
+rm -rf $DIR/$FOLDER
+
 mkdir -p $DIR/$FOLDER && \
   prepare_abx $DIR/egg_edit.csv $DIR/$FOLDER/$NAME --header \
-     --col_features 14-38 --col_labels 6-12 --col_items 1,2,3 && \
+     --col_features 14-38 --col_labels 5-12 --col_items 1,2,3 && \
   run_abx $DIR/$FOLDER/$NAME --on "type" --by place position question speaker vowel &&\
   python dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT && \
   cp $DIR/$FOLDER/abx.csv $DIR/$FOLDER/results.csv; rm $DIR/$FOLDER/abx.csv && \
-  sed -e "s/-/ /g" $DIR/$FOLDER/abx.item > TMP; mv TMP $DIR/$FOLDER/abx.item 
-#&& \
-#  sed -e "s/-/ /g" $OUTPUT_TXT > TMP; mv TMP $OUTPUT_TXT
+  sed -e "s/{}/ /g" $DIR/$FOLDER/abx.item > TMP; mv TMP $DIR/$FOLDER/abx.item && \
+  sed -e "s/{}/ /g" $OUTPUT_TXT > TMP; mv TMP $OUTPUT_TXT
 
 source deactivate
