3f0d4d2545dcb759be5f0c06b038a2042f3cd573
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-19 17:54:54 +0100

    do more tests

--- results/compare_egg.sh
+++ results/compare_egg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp egg_fixed/_avion/results.csv results_egg.csv
+cp egg_fixed/deltas__by__place_position_question_speaker_vowel/results.csv results_deltas.csv
 cp egg_fixed/egg_1000/results.csv results_egg_1000.csv
 
-sed -e "s/, /,/g" results_egg.csv > TMP; mv TMP results_egg.csv
+sed -e "s/, /,/g" results_deltas.csv > TMP; mv TMP results_deltas.csv
 sed -e "s/, /,/g" results_egg_1000.csv > TMP; mv TMP results_egg_1000.csv
--- tests/run_easyabx_features_vot_by__place_position_question_speaker_vowel.sh
+++ tests/run_easyabx_features_vot_by__place_position_question_speaker_vowel.sh
@@ -4,8 +4,8 @@ set -e
 
 source activate easyabx
 
-TEST=egg_fixed
-FOLDER=avion
+TEST=features
+FOLDER=vot_only___by__place_position_question_speaker_vowel
 DIR=../results/$TEST
 
 NAME=abx
@@ -18,17 +18,13 @@ echo
 echo "Running easyabx test and extracting distances"
 echo
 
-rm -rf .cache
-
-rm -rf $DIR/$FOLDER
-
 mkdir -p $DIR/$FOLDER && \
-  prepare_abx $DIR/egg_edit.csv $DIR/$FOLDER/$NAME --header \
-     --col_features 14-38 --col_labels 5-12 --col_items 1,2,3 && \
+  prepare_abx $DIR/abx.csv $DIR/$FOLDER/$NAME --header \
+     --col_features 14 --col_labels 5-12 --col_items 1,2,3 && \
   run_abx $DIR/$FOLDER/$NAME --on "type" --by place position question speaker vowel &&\
   python dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT && \
   cp $DIR/$FOLDER/abx.csv $DIR/$FOLDER/results.csv; rm $DIR/$FOLDER/abx.csv && \
   sed -e "s/{}/ /g" $DIR/$FOLDER/abx.item > TMP; mv TMP $DIR/$FOLDER/abx.item && \
-  sed -e "s/{}/ /g" $OUTPUT_TXT > TMP; mv TMP $OUTPUT_TXT
+  sed -e "s/{}/ /g" $DIR/$FOLDER/distance.txt > TMP; mv TMP $DIR/$FOLDER/distance.txt
 
 source deactivate
